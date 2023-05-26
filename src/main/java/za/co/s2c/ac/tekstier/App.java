f8c107b50f2a13a3997a9b7f54d324dde0fc7cdd
Jan-Rudolph Buhrmann <buhrmannjr@gmail.com>
2023-05-26 13:03:01 +0200

    Removed hard coding of URL

--- src/main/java/za/co/s2c/ac/tekstier/App.java
+++ src/main/java/za/co/s2c/ac/tekstier/App.java
@@ -16,15 +16,18 @@ public class App {
 
         String username = args[0];
         String password = args[1];
+        String url = args[2];
 
-        if (args.length == 3 && args[2].equals ("dev")) {
+//        String url = "https://localhost:8443";
+
+        if (args.length == 4 && args[3].equals ("dev")) {
             HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
         }
 
         String sessionId = null;
         DecryptionClient client = Feign.builder().
                 encoder(new GsonEncoder()).decoder(new GsonDecoder()).
-                target(DecryptionClient.class, "https://localhost:8443");
+                target(DecryptionClient.class, url);
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
@@ -32,7 +35,6 @@ public class App {
             if (next.startsWith(":q"))
                 break;
 
-            // TODO pwd and username from command line
             DecryptionRequestBean request = DecryptionRequestBean.builder().email(username).
                     password(password).appId("f2aac19d-b464-4c04-979e-af1937399f3b").alg("AES").mode("FPE").
                     accountId("85542a03-1574-4e9a-a2ae-587ab16146dc").sessionId(sessionId).cipher(next).build();
